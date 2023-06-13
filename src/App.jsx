385afe0a822a0ed1662986117424fa7ee45dc9ae
Leonardo Diaz <jldiaz9623@gmail.com>
2023-06-13 18:31:26 -0500

    Creacion Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Header from "./components/Header"
+
 function App() {
   return (
-    <>
-      <div className="bg-red-300">
-        Test
-      </div>
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return(
+    <header className="bg-white">
+      <div className="ml-10 mt-8">
+        <h1 className="font-mono font-semibold text-xl">404 NOT FOUND</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
